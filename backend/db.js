5dba548f6541606aa256f8aed862284d4e33b27c
sanir456 <saniranpariya456@gmail.com>
2024-06-08 21:28:24 +0530

    add db.js file

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+mongoose.connect("mongodb+srv://root:1234567890@test.tzv3isg.mongodb.net/")
+
+const todoSchema = mongoose.Schema({
+    title:String,
+    desc:String,
+    completed:Boolean
+})
+
+const todo = mongoose.model("todos",todoSchema)
+
+module.exports = {
+    todo:todo
+}
