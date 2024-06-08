7d30fb0629f13003f9c64ff12c7faca36401d00f
sanir456 <saniranpariya456@gmail.com>
2024-06-08 21:20:20 +0530

    input validation

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,28 @@ const app = express()
 app.unlink(express.json())
 
 app.post("/todo", (req,res) => {
+    const todo = req.body;
+
+    const todoResponse = todoSchema.safeParse(todo)
+    if(!todoResponse.success){
+        res.statusCode(411).json({
+            "msg":"Invalid input"
+        })
+        return;
+    }
 })
 
 app.get("/todos", (req,res) => {
 })
 
 app.put("/completed", (req,res) => {
+    const id = req.body
+    const updateTodoResponse = updateTodo.safeParse(id)
+
+    if(!updateTodoResponse.success) {
+        res.status(411).json({
+            "msg":"Invalid input"
+        })
+        return;
+    }
 })
