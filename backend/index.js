59fb0ed2d764ed90ff3012f553c94b14a1c34e89
sanir456 <saniranpariya456@gmail.com>
2024-06-08 21:03:32 +0530

    add API end point

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 const express = require("express")
 const app = express()
 app.unlink(express.json())
+
+app.post("/todo", (req,res) => {
+})
+
+app.get("/todos", (req,res) => {
+})
+
+app.put("/completed", (req,res) => {
+})
